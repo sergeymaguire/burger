b09eb15f186e800efa51ad668647611e5c29ffc9
sergeymaguire <sergeymaguire@gmail.com>
2018-11-12 10:42:14 -0800

    updating

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,13 @@
 var mysql = require('mysql'),
      connection;
-
-
-     if (process.env.JAWSDB_URL)
-     {
-         connection = mysql.createConnection(process.env.JAWSDB_URL);
-     }
-     else
-     {
-         connection = mysql.createConnection(
+            connection = mysql.createConnection(
          {
              host: 'localhost',
              user: 'root',
              password: '1525wilma',
              database: 'burgers_db'
          });
-     };
-
+ 
   connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
@@ -25,4 +16,3 @@ var mysql = require('mysql'),
     console.log('connected as id ' + connection.threadId);
 });
 
-module.exports = connection;
