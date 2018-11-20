b9fb0a6f3e119a00c9a43f5245c0a21109b74c8e
sergeymaguire <sergeymaguire@gmail.com>
2018-11-20 08:45:11 -0800

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-// Set up MySQL connection.
+
 var mysql = require("mysql");
 if(process.env.JAWS_URL){
   var connection = mysql.createConnection(process.env.JAWS_URL)
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 });
 };
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -21,5 +21,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+
 module.exports = connection;
